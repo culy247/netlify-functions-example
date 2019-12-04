4da77db8e04021d9b18fd1d8a9e6de9e3ead8f92
Jessica Parsons <verythorough@users.noreply.github.com>
2019-12-03 21:45:46 -0800

    Delete hello.js

--- src/lambda/hello.js
+++
@@ -1,7 +0,0 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello there!"
-  });
-  console.log("yup" + process.env.AWS_EXECUTION_ENV)
-};
