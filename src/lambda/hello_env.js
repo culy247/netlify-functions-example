4c90258f7034ec7142c767ac91c7eaa5455183bc
Jessica Parsons <verythorough@gmail.com>
2019-12-03 21:54:58 -0800

    Simplify

--- src/lambda/hello_env.js
+++
@@ -1,8 +0,0 @@
-const { AWS_EXECUTION_ENV } = process.env;
-
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: "AWS Runtime: " + AWS_EXECUTION_ENV
-  };
-};
