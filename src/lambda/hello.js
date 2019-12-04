fa0c13459872b61e3a8a0e91b8d36c1ced700901
Jessica Parsons <verythorough@users.noreply.github.com>
2019-12-03 20:48:49 -0800

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "Hello, " + $AWS_LAMBDA_JS_RUNTIME + "!"
+    body: "Hello there!"
   });
 };
