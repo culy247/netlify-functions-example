72cfbcb3ab9dd0457b2f7a3cd6d2dfbbda16d59e
Jessica Parsons <verythorough@users.noreply.github.com>
2019-12-03 20:53:45 -0800

    Update hello_env.js

--- src/lambda/hello_env.js
+++ src/lambda/hello_env.js
@@ -3,6 +3,6 @@ const { AWS_LAMBDA_JS_RUNTIME } = process.env;
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: AWS_LAMBDA_JS_RUNTIME
+    body: "Runtime: " + AWS_LAMBDA_JS_RUNTIME
   };
 };
